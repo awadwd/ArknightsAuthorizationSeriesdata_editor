006fdab63105b7d19349cdf338914ac52a925d9f
awadwd <2726269007@qq.com>
2026-06-04 17:26:31 +0800

    Fix: also expose tmFeatureList in setup() return

--- src/App.vue
+++ src/App.vue
@@ -638,7 +638,7 @@ export default {
 
   setup() {
     const { locale, localeOptions, setLocale } = useI18n()
-    return { locale, localeOptions, setLocale, tm }
+    return { locale, localeOptions, setLocale, tm, tmFeatureList }
   },
 
   data() {
