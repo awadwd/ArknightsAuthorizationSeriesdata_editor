3ab1a8cf044030fc2b9f24370d6590dee5f07626
awadwd <111214359+awadwd@users.noreply.github.com>
2026-09-18 18:54:36 +0800

    sync: update kc-data from 知晓云 @ 2026-09-18T10:54:35.398Z

--- functions/api/kc-data.js
+++ functions/api/kc-data.js
@@ -237,7 +237,7 @@ export const KC_DATA = {
   ],
   "AiToolsConfig": [
     {
-      "Enabled": false,
+      "Enabled": true,
       "_id": "6996dc2d965f3bcec9cee078",
       "created_at": 1771494445,
       "created_by": 91866855,
@@ -245,7 +245,7 @@ export const KC_DATA = {
       "read_perm": [
         "user:anonymous"
       ],
-      "updated_at": 1789657239,
+      "updated_at": 1789728861,
       "write_perm": []
     }
   ]
